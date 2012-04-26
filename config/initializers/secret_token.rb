173768efc4bb926f90fd8af913e0026a00a59a24
koshikawa <koshikawa@ppworks.jp>
2012-04-26 10:41:30 +0900

    rails new p4dhack -T -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P4dhack::Application.config.secret_token = '42e5bf787c7ad63a7c6debc8e375ac5fa04fe8df7a69f59c7adc372e8e8248c9ca0b93518b3ede0567077752e1ed545967dd15528e0d0387e8ed38ddad5a0160'

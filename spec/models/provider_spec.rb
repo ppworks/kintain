0d55c2ff447af900b0a2f8141ffa107e7f3d17d8
koshikawa <koshikawa@ppworks.jp>
2012-05-05 23:21:41 +0900

    mixi対応spec更新

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe Provider do
-  it 'provider has 3 providers' do
-    Provider.all.count.should == 3
+  it 'provider has 4 providers' do
+    Provider.all.count.should == 4
   end
   
   describe 'providers' do
     it { Provider.facebook.should be_present }
     it { Provider.twitter.should be_present }
     it { Provider.github.should be_present }
+    it { Provider.mixi.should be_present }
   end
 end
